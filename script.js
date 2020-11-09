3f7fc696137bed053c3da6c6999ca066c354806c
Gorm <gormroedder@gmail.com>
2020-11-09 22:09:03 +0100

    heatmap alpha

--- script.js
+++ script.js
@@ -11,12 +11,22 @@ const startDate = new Date(2020, 8, 1)
 const w = 600
 const h = 300
 const margin = { top: 50, bottom: 25, left: 5, right: 35 }
+const colorOfBad = "#b01b81"
 
 let maxOverall
 
+
+
+// const xBand = d3.scaleBand()
+//   .domain(d3.timeDays(startDate, new Date().setHours(0,0,0)))
+//   .range([margin.left, w - margin.right])
+//   .padding(0.1)
+
 const x = d3.scaleTime()
-  .domain([startDate, new Date().setHours(0, 0, 0)])
-  .range([margin.left, w - margin.right])
+  .domain([startDate, d3.timeDay.offset(new Date(), 0)])
+  .rangeRound([margin.left, w - margin.right])
+
+const bandwidth = (x(d3.timeDay.offset(startDate, 1)) - x(startDate)) * 0.8
 
 const emptyTimeline = d3.timeDays(startDate, new Date()).map(d => {
   return { 
@@ -66,7 +76,7 @@ prepare = (data) => {
 getFacultiesTrondheim = data => {
   return [...new Set(data.filter(d => d["By"] == "Trondheim").map(d => d["Avdeling"]))].reduce((result, item) => {
     if (item) {
-      result[`${item} Trondheim`] = prepare(data.filter(d => d["Avdeling"] == item && d["By"] == "Trondheim"))
+      result[item] = prepare(data.filter(d => d["Avdeling"] == item && d["By"] == "Trondheim"))
     }
     return result
   }, {})
@@ -83,12 +93,11 @@ const getMonthInNorwegian = date => {
   return month.substring(0,1).toUpperCase() + month.substring(1)
 }
 
-const drawSection = (d, group) => {
-  const chart = d3.select("#chart")
-  const requiredHeight = calculateHeight(d)
+const drawSection = (cityDiv, data, group) => {
+  const requiredHeight = calculateHeight(data)
 
   const y = d3.scaleLinear()
-    .domain([0, d[1].maxPerDay])
+    .domain([0, data[1].maxPerDay])
     .range([requiredHeight - margin.bottom, margin.top])
 
   const area = d3.area()
@@ -98,23 +107,31 @@ const drawSection = (d, group) => {
 
   const line = area.lineY1()
 
-  const svg = chart.append('svg')
+  const svg = cityDiv.append('svg')
+    .classed('city', true)
     .attr('viewBox', `0 0 ${w} ${requiredHeight}`)
-      // .attr('width', w)
-      // .attr('height', h)
 
-  svg.append('path')
-    .attr('d', area(d[1][group]))
-    .attr('fill', ntnuSupportColorsLighter[1])
+  // svg.append('path')
+  //   .attr('d', area(d[1][group]))
+  //   .attr('fill', ntnuSupportColorsLighter[1])
   
-  svg.append('path')
-    .attr('d', line(d[1][group]))
-    .attr('fill', 'none')
-    .attr('stroke', ntnuBlue)
+  // svg.append('path')
+  //   .attr('d', line(d[1][group]))
+  //   .attr('fill', 'none')
+  //   .attr('stroke', ntnuBlue)
+
+  svg.selectAll('rect')
+    .data(data[1][group]).enter()
+    .append('rect')
+      .attr('fill', colorOfBad)
+      .attr('x', d => x(d.date))
+      .attr('y', d => y(d.value))
+      .attr('height', d => y(0) - y(d.value))
+      .attr('width', bandwidth)
 
   svg.append('g') // cases
     .attr("transform", `translate(${margin.left},0)`)
-    .call(d3.axisRight(y).ticks(d[1]['maxPerDay']/maxOverall * 4)
+    .call(d3.axisRight(y).ticks(data[1]['maxPerDay']/maxOverall * 4)
       .tickSize(w - margin.left - margin.right))
     .call(g => g.select(".domain")
       .remove())
@@ -137,36 +154,61 @@ const drawSection = (d, group) => {
     .classed('title', true)
     .attr('x', margin.left)
     .attr('y', margin.top - 30)
-    .text(d[0])
+    .text(data[0])
 
   svg.append('text')
     .classed('total', true)
     .attr('x', margin.left)
     .attr('y', margin.top - 15)
-    .text(`Totalt antall innmeldte smittede ${group}: ${d[1]['studenterSum']}`)
+    .text(`Totalt antall innmeldte smittede ${group}: ${data[1]['studenterSum']}`)
 }
 
 
 
-const drawHeatmap = data => {
-  const chart = d3.select("#chart")
+const drawHeatmap = (cityDiv, data, group, heatmapColorScale) => {
+  svg = cityDiv.append('svg')
+    .classed('heat', true)
+    .attr('viewBox', `0 0 ${w} 12`)
 
-  chart.append('div').text("test")
+  svg.selectAll('rect')
+    .data(data[1][group]).enter()
+    .append('rect')
+      .attr('x', d => x(d.date))
+      .attr('y', 0)
+      .attr('height', 12)
+      .attr('width', bandwidth)
+      .attr('fill', d => heatmapColorScale(d.value))
+  
+  // svg.append('line')
+  //   .attr('x1', margin.left)
+  //   .attr('y1', 11)
+  //   .attr('x2', w - margin.right)
+  //   .attr('y2', 11)
+  //   .attr('stroke', 'gray')
+
+  svg.append('text')
+    .classed('fac', true)
+    .attr('x', w - margin.right + 10)
+    .attr('y', 10)
+    .text(data[0])
 }
 
 drawChart = (data, group) => {
   
 
   Object.entries(data).forEach(d => {
-    drawSection(d, group)
+    const cityDiv = d3.select('#chart').append('div').classed('city-div', true)
+
+
+    drawSection(cityDiv, d, group)
     if (d[1].fak) {
       const faculties = Object.entries(d[1].fak).sort((a,b) => b[1]['maxPerDay'] - a[1]['maxPerDay'])
       const heatmapColorScale = d3.scaleLinear()
         .domain([0, d3.max(faculties, f => f[1]['maxPerDay'])])
-        .range(["white", "red"])
+        .range(["white", colorOfBad])
 
       faculties.forEach(f => {
-        drawHeatmap(f)
+        drawHeatmap(cityDiv, f, group, heatmapColorScale)
       })
     }
   })
